c97a81ad1efb7f2c94ed0ae24db0a497ac84c473
mlxiao <mlxiao93@163.com>
2017-02-09 10:32:25 +0800

    test async and await

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import './index.scss'
 
 import config from 'config'
 
-console.log(config);
+let foo = async () => {
+  let promise = new Promise(reslove => {
+    setTimeout(() => {
+      reslove(config);
+    }, 2000)
+  })
+  let res = await promise;
+  return Promise.resolve(res);
+};
+
+foo().then(res => console.log(res));

67a4973e59347760875cb11e8209096f603b04bd
肖茂林 <xiaomaolin@meituan.com>
2017-03-20 13:47:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,14 +2,4 @@ import './index.scss'
 
 import config from 'config'
 
-let foo = async () => {
-  let promise = new Promise(reslove => {
-    setTimeout(() => {
-      reslove(config);
-    }, 2000)
-  })
-  let res = await promise;
-  return Promise.resolve(res);
-};
-
-foo().then(res => console.log(res));
+console.log(config);
